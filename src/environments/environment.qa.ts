6469a5c892dfb52ceb67c6b5137ce65c8d4c5515
Alexander <balexander.rosas@gmail.com>
2025-06-20 00:38:09 -0600

    Envs nuevo

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,7 +1,4 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// noinspection JSUnusedGlobalSymbols
 
 import swal from 'sweetalert2';
 
@@ -9,10 +6,11 @@ export const environment = {
     production: false,
 };
 
-export const backend_url = 'http://localhost:8000/';
+export const backend_url = 'https://rest.afainnova.com/';
+export const printserver_url = 'http://psafa-test.ddns.net:2221/';
 export const backend_url_password =
     '$2y$10$zUFltp9AVApnk7BN22Nu9ueCvBihctYkDFJLvN0HlVaBr4KYtRnfy';
-export const pusher_key = 'd2163e9e88304df4c43e';
+// export const pusher_key = 'd2163e9e88304df4c43e';
 export const dropbox_token =
     'AYQm6f0FyfAAAAAAAAAB2PDhM8sEsd6B6wMrny3TVE_P794Z1cfHCv16Qfgt3xpO';
 export const raspberry_dyndns =
@@ -21,7 +19,7 @@ export const mercadolibre_url = 'https://api.mercadolibre.com/';
 export const dropbox_api_url = 'https://api.dropboxapi.com/2/';
 
 export function commaNumber(number) {
-    var parts = number.toString().split('.');
+    const parts = number.toString().split('.');
 
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ', ');
 
@@ -29,6 +27,7 @@ export function commaNumber(number) {
 }
 
 export function swalErrorHttpResponse(err) {
+    console.log(err);
     swal({
         title: '',
         type: 'error',
@@ -36,19 +35,20 @@ export function swalErrorHttpResponse(err) {
             err.status == 0
                 ? err.message
                 : typeof err.error === 'object'
-                ? err.error.error_summary
                     ? err.error.error_summary
-                    : err.error.message
-                : err.error,
-    });
+                        ? err.error.error_summary
+                        : err.error.message
+                    : err.error,
+    }).then();
 }
+
 
 export function swalSuccessHttpResponse(res) {
     swal({
         title: '',
         type: 'success',
         html: res.message,
-    });
+    }).then();
 }
 
 export function downloadExcelReport(excel_name: string, excel_data: string) {
@@ -75,17 +75,3 @@ export function downloadPDF(pdf_name: string, pdf_data: string) {
     a.click();
 }
 
-export const tinymce_init = {
-    theme: 'modern',
-    height: 300,
-    plugins: [
-        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-        'searchreplace wordcount visualblocks visualchars code fullscreen',
-        'insertdatetime media nonbreaking save table contextmenu directionality',
-        'emoticons template paste textcolor colorpicker textpattern imagetools',
-    ],
-    toolbar1:
-        'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    toolbar2: 'print preview media | forecolor backcolor emoticons',
-    automatic_uploads: true,
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,11 @@ export const environment = {
     production: false,
 };
 
-export const backend_url = 'http://afa.spaxium.com:16227/';
+export const backend_url = 'https://rest.afainnova.com/';
 export const printserver_url = 'http://psafa-test.ddns.net:2221/';
 export const backend_url_password =
     '$2y$10$zUFltp9AVApnk7BN22Nu9ueCvBihctYkDFJLvN0HlVaBr4KYtRnfy';
-export const pusher_key = 'd2163e9e88304df4c43e';
+// export const pusher_key = 'd2163e9e88304df4c43e';
 export const dropbox_token =
     'AYQm6f0FyfAAAAAAAAAB2PDhM8sEsd6B6wMrny3TVE_P794Z1cfHCv16Qfgt3xpO';
 export const raspberry_dyndns =
@@ -22,7 +22,7 @@ export const mercadolibre_url = 'https://api.mercadolibre.com/';
 export const dropbox_api_url = 'https://api.dropboxapi.com/2/';
 
 export function commaNumber(number) {
-    var parts = number.toString().split('.');
+    const parts = number.toString().split('.');
 
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ', ');
 
@@ -38,11 +38,11 @@ export function swalErrorHttpResponse(err) {
             err.status == 0
                 ? err.message
                 : typeof err.error === 'object'
-                ? err.error.error_summary
                     ? err.error.error_summary
-                    : err.error.message
-                : err.error,
-    });
+                        ? err.error.error_summary
+                        : err.error.message
+                    : err.error,
+    }).then();
 }
 
 
@@ -51,7 +51,7 @@ export function swalSuccessHttpResponse(res) {
         title: '',
         type: 'success',
         html: res.message,
-    });
+    }).then();
 }
 
 export function downloadExcelReport(excel_name: string, excel_data: string) {
@@ -77,18 +77,3 @@ export function downloadPDF(pdf_name: string, pdf_data: string) {
 
     a.click();
 }
-
-export const tinymce_init = {
-    theme: 'modern',
-    height: 300,
-    plugins: [
-        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-        'searchreplace wordcount visualblocks visualchars code fullscreen',
-        'insertdatetime media nonbreaking save table contextmenu directionality',
-        'emoticons template paste textcolor colorpicker textpattern imagetools',
-    ],
-    toolbar1:
-        'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    toolbar2: 'print preview media | forecolor backcolor emoticons',
-    automatic_uploads: true,
-};
